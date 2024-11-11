b256f225ed80c63c752db77c5d94df333b5761a0
abdul-barii <abdulbari3088@gmail.com>
2024-11-11 15:24:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,9 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (mapping[char]) {
+            const topElement = stack.length === 0 ? '#' : stack.pop();
+            if (topElement !== mapping[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
